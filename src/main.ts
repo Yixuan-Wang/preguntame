64411d08c5d67781a04f4b0498f9fc64d3bb394e
Yixuan-Wang <21151119+Yixuan-Wang@users.noreply.github.com>
2021-08-23 13:46:40 +0800

    fix: router base url

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ import { useCoreStore } from './stores'
 
 const app = createApp(App)
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 const head = createHead()
