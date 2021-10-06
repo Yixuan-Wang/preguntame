fa7df0389476f52d13661f588a4ee63d25873172
Yixuan-Wang <Yixuan-Wang@users.noreply.github.com>
2021-10-06 06:51:52 +0000

    deploy: 8608b332cb2af352811069b95d7ec32681798c99

--- assets/[...all].6bc5eb17.js
+++
@@ -1,1 +0,0 @@
-import{o,c as n}from"./vendor.2ef4c889.js";var r={};const t={};"function"==typeof r&&r(t),t.render=function(r,t){return o(),n("div",null," Not Found ")};export default t;
--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/preguntame/sw.js', { scope: '/preguntame/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
