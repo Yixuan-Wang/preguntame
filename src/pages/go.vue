faf249add8a05356510cacf3869d3ea76f7c44a5
Yixuan-Wang <21151119+Yixuan-Wang@users.noreply.github.com>
2021-10-06 13:16:17 +0800

    feat: go page

---
+++ src/pages/go.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { useCoreStore } from '~/stores'
+import { toQuery } from '~/composables/parse'
+import { launch } from '~/composables/launch'
+
+const route = useRoute()
+launch(toQuery(route.query?.q as string ?? ''), useCoreStore())
+</script>
